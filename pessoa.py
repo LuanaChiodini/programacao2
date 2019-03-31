e604c75898c6147983926700d20cdaa014ac6165
Luana <47819569+LuanaChiodini@users.noreply.github.com>
2019-03-31 20:59:31 -0300

    Delete pessoa.py

--- pessoa.py
+++
@@ -1,9 +0,0 @@
-class Pessoa():
-	def __init__(self, nome, endereco, telefone):
-		self.nome = nome
-		self.endereco = endereco
-		self.telefone = telefone
-
-if __name__ == "__main__":
-	pessoa = Pessoa("Maria", "rua", "1111-1111") 
-	print(pessoa.nome, pessoa.endereco, pessoa.telefone)
