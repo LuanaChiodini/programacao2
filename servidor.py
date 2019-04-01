94b193ac982afb4214d64057fcc2165ab193991c
Luana <47819569+LuanaChiodini@users.noreply.github.com>
2019-03-31 22:55:16 -0300

    Add files via upload
    adicionando a rota /excluir_pessoa no servidor

--- servidor.py
+++ servidor.py
@@ -1,46 +1,43 @@
-from flask import Flask, render_template, request
+import Flask, render_template, request
 
+class Pessoa():
+	def __init__(self, nome, endereco, cpf):
+		self.nome = nome
+		self.endereco = endereco
+		self.cpf = cpf
 
 app = Flask(__name__)
+pessoas = []
 
 @app.route("/")
 def inicio():
 	return render_template("inicio.html")
 
-pessoas = []
-
-class Pessoa():
-	def __init__(self, nome, endereco, telefone):
-		self.nome = nome
-		self.endereco = endereco
-		self.telefone = telefone
-
 @app.route("/listar_pessoas")
-def listar_pessoas():
+def listar():
 	return render_template("listar_pessoas.html", lista=pessoas)
 
 @app.route("/cadastrar_pessoa")
-def adicionar():
+def casdastrar_pessoa():
 	nome = request.args.get("nome")
 	endereco = request.args.get("endereco")
-	telefone = request.args.get("telefone")
-	pessoas.append(Pessoa(nome, endereco, telefone))
-	return render_template("exibir_mensagem.html", pessoa=(nome,endereco,telefone)) 
-
-@app.route("/form_inserir_pessoa")
-def form_inserir_pessoa():
-	return render_template("form_inserir_pessoa.html")
-
-@app.route("/form_excluir_pessoa")
-def form_excluir_pessoa():
-	return render_template("form_excluir_pessoa.html")
+	cpf = request.args.get("cpf")
+	pessoas.append(Pessoa(nome, endereco, cpf))
+	return render_template("exibir_mensagem.html", mensagem="cadastro concluído", pessoa=(nome,endereco,cpf))
 
 @app.route("/form_alterar_pessoa")
-def form_alterar_pessoa():
-	return render_template("form_alterar_pessoa.html")
+def alterar_pessoa():
+	return render_template("exibir_mensagem.html", mensagem="pessoa alterada")
 
-@app.route("/exibir_mensagem")
-def exibir_mensagem():
-	return render_template("exibir_mensagem.html")
+@app.route("/form_excluir_pessoa")
+def excluir_pessoa():
+	achou = None
+	cpf = request.args.get("cpf")
+	for p in pessoas:
+		if p.cpf == cpf:
+			pessoas.remove(achou)
+	return render_template("exibir_mensagem.html", mensagem="pessoa excluída")
 
-app.run(debug=True, host="0.0.0.0")
+app.run()
+
+app = run(debug=True, host="0.0.0.0")
