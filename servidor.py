84b38e3f53fb03b174cd9fa825f5c2b555e834d3
LuanaChiodini <lchiodini87@gmail.com>
2019-04-11 11:15:36 -0300

    alterar

--- servidor.py
+++ servidor.py
@@ -1,4 +1,4 @@
-import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 class Pessoa():
 	def __init__(self, nome, endereco, cpf):
@@ -25,16 +25,37 @@ def casdastrar_pessoa():
 	pessoas.append(Pessoa(nome, endereco, cpf))
 	return render_template("exibir_mensagem.html", mensagem="cadastro concluído", pessoa=(nome,endereco,cpf))
 
-@app.route("/form_alterar_pessoa")
-def alterar_pessoa():
-	return render_template("exibir_mensagem.html", mensagem="pessoa alterada")
+@app.route("/form_inserir_pessoa")
+def form_inserir_pessoa():
+	return render_template("/form_inserir_pessoa.html")
 
 @app.route("/form_excluir_pessoa")
-def excluir_pessoa():
+def form_excluir_pessoa():
 	cpf = request.args.get("cpf")
 	for p in pessoas:
 		if p.cpf == cpf:
 			pessoas.remove(p)
 	return render_template("exibir_mensagem.html", mensagem="pessoa excluída")
+
+@app.route("/form_alterar_pessoa")
+def form_alterar_pessoa():
+	cpf = request.args.get("cpf")
+	for p in pessoas:
+		if p.cpf == cpf:
+			return render_template("form_alterar_pessoa.html", informacao=p)
+	return "pessoa" + p + "excluída"
+
+@app.route("/alterar_pessoa")
+def alterar_pessoa():
+	procurado = request.args.get("cpf_original")
+	nome = request.args.get("nome")
+	endereco = request.args.get("endereco")
+	cpf = request.args.get("cpf")
+	nova_pessoa = Pessoa(nome, endereco, cpf)
+	for i in range(len(pessoas)):
+		if pessoas[i].cpf == procurado:
+			pessoas[i] = nova_pessoa
+			return redirect("/listar_pessoas")
+	return "pessoa não encontrada"
 
 app.run(debug=True, host="0.0.0.0")
