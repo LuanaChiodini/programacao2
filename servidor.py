8951bb6b59c3af5bf5c0ea497259da97b692e451
LuanaChiodini <lchiodini87@gmail.com>
2019-04-24 10:21:44 -0300

    controle acesso

--- servidor.py
+++ servidor.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 
 class Pessoa():
 	def __init__(self, nome, endereco, cpf):
@@ -7,6 +7,7 @@ class Pessoa():
 		self.cpf = cpf
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "2243"
 pessoas = []
 
 @app.route("/")
@@ -31,11 +32,12 @@ def form_inserir_pessoa():
 
 @app.route("/form_excluir_pessoa")
 def form_excluir_pessoa():
-	cpf = request.args.get("cpf")
-	for p in pessoas:
-		if p.cpf == cpf:
-			pessoas.remove(p)
-	return render_template("exibir_mensagem.html", mensagem="pessoa excluída")
+	if session["usuario"]:
+		cpf = request.args.get("cpf")
+		for p in pessoas:
+			if p.cpf == cpf:
+				pessoas.remove(p)
+		return render_template("exibir_mensagem.html", mensagem="pessoa excluída")
 
 @app.route("/form_alterar_pessoa")
 def form_alterar_pessoa():
@@ -57,5 +59,23 @@ def alterar_pessoa():
 			pessoas[i] = nova_pessoa
 			return redirect("/listar_pessoas")
 	return "pessoa não encontrada"
+
+@app.route("/form_login")
+def form_login():
+	return render_template("form_login.html")
+
+@app.route("/login")
+def login():
+	login = request.args.get("login")
+	senha = request.args.get("senha")
+	if login == "luana" and senha == "1234":
+		return redirect("/")
+	else:
+		return "login e/ou senha inválidos"
+
+@app.route("/logout")
+def logout();
+	session.pop("usuario")
+	return redirect("/")
 
 app.run(debug=True, host="0.0.0.0")
