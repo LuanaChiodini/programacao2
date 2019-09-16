51a79334d355c38447630000f53875a0cb24d048
LuanaChiodini <lchiodini87@gmail.com>
2019-09-16 13:10:26 -0300

    16 de set

--- integracao/front/servidor.py
+++ integracao/front/servidor.py
@@ -9,4 +9,14 @@ app = Flask(__name__)
 def inicio():
 	return "frontend do sistema de pessoa. <a href=/listar_pessoas>Operação listar</a>"
 
-@app.route("/listart")
+@app.route("/listar_pessoas")
+def listar_pessoas():
+	dados_pessoas = requests.get("http://localhost:4999/listar_pessoas")
+	json_pesssoas = dados_pessoas.json()
+	pessoas = []
+	for pessoa_em_json in json_pesssoas["lista"]:
+		p = dict_to_model(Pessoa, pessoa_em_json)
+		pessoas.append(p)
+	return render_template("listar_pessoas.html", lista=pessoas)
+
+app.run(debug=True)	
---
+++ integracao/integrado/servidor.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+from modelo import Pessoa
+
+app = Flask(__name__)
+
+@app.route("/")
+def inicio():
+    return "sistema de pessoas. <a href=/listar_pessoas>Operação listar</a>"
+
+@app.route("/listar_pessoas")
+def listar_pessoas():
+    return render_template("listar_pessoas.html", lista = Pessoa.select())
+
+app.run(debug=True)
